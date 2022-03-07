64f142bf516c9ba9013aed9d2365ad0510b7dbaa
Thomas <thomas.bogle@outlook.com>
2022-03-07 14:43:09 +0000

    Naming Pets

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -4,6 +4,11 @@ describe('constructor', () => {
     it('returns an object', () => {
       expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
+    it('sets the name property', () => {
+      const pet = new Pet('Fido');
+  
+      expect(pet.name).toEqual('Fido');
+    });
   });
 
 
