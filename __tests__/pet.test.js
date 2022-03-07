d587a69dfcf7deb9739df700a4a3254da49c56cf
Thomas <thomas.bogle@outlook.com>
2022-03-07 22:34:08 +0000

    Getting older, getting unhealthier

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -14,13 +14,37 @@ describe('constructor', () => {
 
       expect(pet.age).toEqual(0);
     });
-    it('increases the age of pet by 1', () => {
+    it('pet has an initial hunger of 0', () => {
+      const pet = new Pet('Fido');
+
+      expect(pet.hunger).toEqual(0);
+    });
+    it('pet has an initial fitness of 10', () => {
+      const pet = new Pet('Fido');
+
+      expect(pet.fitness).toEqual(10);
+    });
+    it('increases age of pet by 1', () => {
       const pet = new Pet('Fido');
 
       pet.growUp();
 
       expect(pet.age).toEqual(1);
-    }); 
+    });
+    it('increases hunger of pet by 5', () => {
+      const pet = new Pet('Fido');
+
+      pet.growUp();
+
+      expect(pet.hunger).toEqual(5);
+    });
+    it('decreases fitness of pet by 3', () => {
+      const pet = new Pet('Fido');
+
+      pet.growUp();
+
+      expect(pet.fitness).toEqual(7);
+    });
   });
 
 
